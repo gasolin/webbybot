eb2f21b083ea14f595126b44827f399a9340e252
gasolin <gasolin@gmail.com>
2016-02-16 21:04:28 +0800

    use webpack to compile es6

---
+++ README.md
@@ -0,0 +1,24 @@
+# Webbybot
+
+
+## Setup Development
+
+```
+$ npm install -g webpack
+```
+
+## Build
+
+run command
+
+```
+$ webpack
+```
+
+## Run
+
+run command
+
+```
+$ ./bin/webby.js
+```
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bin/webby.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+};
