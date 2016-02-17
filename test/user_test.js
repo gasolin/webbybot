c6ebcb0ca3740b5bbb2ee46d0ec1567b5c3b8673
gasolin <gasolin@gmail.com>
2016-02-17 13:32:12 +0800

    polish user_test

--- test/user_test.js
+++ test/user_test.js
@@ -6,25 +6,27 @@ var User = require('../src/user');
 describe('User', function() {
   return describe('new', function() {
     it('uses id as the default name', function() {
-      var user;
-      user = new User('hubot');
-      return expect(user.name).to.equal('hubot');
+      var user = new User('hubot');
+
+      expect(user.name).to.equal('hubot');
     });
+
     it('sets attributes passed in', function() {
-      var user;
-      user = new User('hubot', {
+      var user = new User('hubot', {
         foo: 1,
         bar: 2
       });
+
       expect(user.foo).to.equal(1);
-      return expect(user.bar).to.equal(2);
+      expect(user.bar).to.equal(2);
     });
-    return it('uses name attribute when passed in, not id', function() {
-      var user;
-      user = new User('hubot', {
+
+    it('uses name attribute when passed in, not id', function() {
+      var user = new User('hubot', {
         name: 'tobuh'
       });
-      return expect(user.name).to.equal('tobuh');
+
+      expect(user.name).to.equal('tobuh');
     });
   });
 });
