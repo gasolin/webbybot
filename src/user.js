309ec24f3a3285751edcad7d314ea20812a5407a
gasolin <gasolin@gmail.com>
2016-03-30 13:53:57 +0800

    add comment for user module

--- src/user.js
+++ src/user.js
@@ -1,3 +1,9 @@
+/**
+ * Represents a participating user in the chat.
+ *
+ * @param {string} id      - A unique ID for the user.
+ * @param {object} options - An optional Hash of key, value pairs for this user.
+ */
 class User {
   constructor(id, options = {}) {
     this.id = id;
